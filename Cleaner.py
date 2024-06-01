8926cf89f861c41eff63868fb3fd2c69f29cdf26
Shell <157921830+Shellteur@users.noreply.github.com>
2024-06-01 17:17:10 +0200

    Update Cleaner.py

--- Cleaner.py
+++ Cleaner.py
@@ -6,10 +6,11 @@ import time
 # Gather the source destination directory and whatever other directory you want to sort your files in.
 # Specify them with a variable
 #~~~~~~
-source = r'C:\Users\poeti\Desktop\magic_hole' # Source directory (The magic hole)
-destination_video = r'C:\Users\poeti\Desktop\Krims Krams\meemu2024' # video
-destination_picture = r'C:\Users\poeti\Pictures\Pictures' # picture
-destination_sound = r'C:\Users\poeti\Desktop\Krims Krams\Musik\musika' #sound(music)
+source = r'Src file here' # Source directory (The magic hole)
+destination_video = r'file here' # video
+destination_picture = r'file here' # picture
+destination_sound = r'file here' #sound(music)
+#Add as many categories you want!
 #~~~~~~
 # Filter the files with Global and add the prefixes you want like mp3,MOV etc
 videos = glob.glob(os.path.join(source, '*.mp4*'), recursive=True)
@@ -18,6 +19,7 @@ sound = glob.glob(os.path.join(source, '*.mp3*'), recursive=True)
 #~~~~~~
 # Finally use the directory variable and combine its coresponding filter variable
 # Loop until all files have been moved
+#Add as many categories you want!
 time.sleep(0.33)
 for file_path in videos:
     dst_path = os.path.join(destination_video, os.path.basename(file_path))
@@ -35,3 +37,5 @@ for file_path in sound:
     dst_path = os.path.join(destination_sound, os.path.basename(file_path))
     shutil.move(file_path, dst_path)
     print(f"Moved {file_path} -> {dst_path}")
+
+#Add as many categories you want!
